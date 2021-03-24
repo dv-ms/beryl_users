33476c16537030890cd763e3069d412f8e490621
Your Name <you@example.com>
2021-03-24 11:30:50 +0530

    Removed unwanted style tag

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export default class Home extends Component {
   render() {
     return (
-      <div style={{}}>
+      <div>
         <Link to="/users">
           <Button variant="success">USERS</Button>
         </Link>
